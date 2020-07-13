d2937752a4fcba29393d7a945878825d2c4f42b1
ritsulog <ritsulog@gmail.com>
2020-07-14 02:15:49 +0900

    develop up

---
+++ watch_test.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+WIN_X = 1024
+WIN_Y = 600
+WIN_GEOMETRY = "{}x{}".format(WIN_X, WIN_Y)
+X_DIVIDE_block = WIN_X / 10
+Y_DIVIDE_block = WIN_Y / 3
+
+class Application(tk.Frame):
+    def __init__(self,master):
+        super().__init__(master, bg="black")
+        self.pack(anchor=tk.NW) #左寄せ
+
+        self.master.geometry(WIN_GEOMETRY)
+        self.master.title("Watch")
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        frame = tk.Frame(self, bg="white", width=8*X_DIVIDE_block, height=2*Y_DIVIDE_block, bd=0, relief="flat")
+        frame.pack()
+
+    def callBack(self):
+        pass
+
+def main():
+    root = tk.Tk()
+    app = Application(master=root)  #Inherit
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()    
